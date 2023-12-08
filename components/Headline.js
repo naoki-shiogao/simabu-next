0941b6db01360afd62305e9cae1c68e5b766f2af
naoki-shiogao <hors1359@gmail.com>
2023-12-08 13:24:22 +0900

    ♻️ Headline部分をコンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,15 @@
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Headline(props) {
+  return (
+    <>
+      <p>
+        Get started by editing&nbsp;
+        <code className={styles.code}>{props.title}</code>
+      </p>
+    </>
+  );
+}
