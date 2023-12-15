21722ef9ffe5b5225082417259240858edefa92a
naoki-shiogao <hors1359@gmail.com>
2023-12-15 17:56:30 +0900

    ✨ ページ間のstateのリフトアップについて

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,20 @@
 import "@/src/styles/globals.css";
 import Head from "next/head";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLiteBlue } from "@/src/hooks/useBgLiteBlue";
 
 export default function App({ Component, pageProps }) {
+  const counter = useCounter();
+  const inputArray = useInputArray();
+  useBgLiteBlue();
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} {...counter} {...inputArray} />
     </>
   );
 }
