3b27fe624a85b38282dedb72d0487ad35573bc3d
naoki-shiogao <hors1359@gmail.com>
2023-12-20 21:20:42 +0900

    ✨ ユーザーのfetch,fetcherの共通化

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,27 @@
+import { usePost } from "@/src/hooks/usePost";
+import Head from "next/head";
+
+export const Post = () => {
+  const { user, post, error, isLoading } = usePost();
+
+  if (isLoading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  return (
+    <>
+      <div>
+        <Head>
+          <title>{post?.title}</title>
+        </Head>
+        <h1>{post?.title}</h1>
+        <p>{post?.body}</p>
+        {user?.name ? <div>Created by {user.name}</div> : null}
+      </div>
+    </>
+  );
+};
