c84e9215b902df5b962ddab73d3399a0d3b57134
naoki-shiogao <hors1359@gmail.com>
2023-12-22 10:52:11 +0900

    ✨ ユーザーページに投稿一覧を追加

---
+++ src/components/Posts/PostsByUserId.jsx
@@ -0,0 +1,30 @@
+import { usePostsByUserId } from "@/src/hooks/useFetchArray";
+import Link from "next/link";
+
+export const PostsByUserId = (props) => {
+  const { data, error, isLoading, isEmpty } = usePostsByUserId(props.id);
+
+  if (isLoading) {
+    return <div>ローディング中</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
+  if (isEmpty) {
+    return <div>データは空です</div>;
+  }
+
+  return (
+    <>
+      <ol>
+        {data.map((item) => (
+          <li key={item.id}>
+            <Link href="/">{item.title}</Link>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+};
